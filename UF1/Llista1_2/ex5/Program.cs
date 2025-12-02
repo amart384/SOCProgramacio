9cac876ffb0a045b0a7e705fe3e6709b76e94c98
amart384 <amart384@institutmontilivi.cat>
2025-12-02 15:47:31 +0100

    v1

---
+++ UF1/Llista1_2/ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Llista1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lletra_int; //16 bits 
+            char lletra_char; //32bits   
+
+            //Console.WriteLine("Entra una lletra:");
+            //lletra_char = Convert.ToInt32(Console.ReadLine()); //ERROR
+            //Console.WriteLine("lletra_char  " + lletra_char);
+
+            //Exercici 5. No funciona
+            Console.WriteLine("Entra una lletra:");
+            lletra_char = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("lletra_char  "+lletra_char); //mostra a
+
+            //Exercici 5. Sí funciona
+            Console.WriteLine("Entra una lletra:");
+            lletra_int = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("lletra_char  "+ lletra_int); //mostra 97
+
+        }
+    }
+}
---
+++ UF1/Llista1_2/ex6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int majuscules_int;
+            char majuscules_char;
+            int lletra_int; //16 bits 
+
+            //Exercici 6
+            Console.WriteLine("Entra una lletra:");
+            lletra_int = Convert.ToChar(Console.ReadLine());
+            //passa a majuscula
+            majuscules_int = lletra_int - 32;
+            Console.WriteLine($"majuscules {majuscules_int}");
+            //conversio a caracter
+            majuscules_char = Convert.ToChar(majuscules_int);
+            Console.WriteLine($"majuscules {majuscules_char}");
+
+            //Exercici 6. Opció2
+            Console.WriteLine("Entra una lletra:");
+            string letra = Console.ReadLine();
+            string letra_mayuscula = letra.ToUpper();
+            Console.WriteLine(letra_mayuscula);
+
+        }
+    }
+}
